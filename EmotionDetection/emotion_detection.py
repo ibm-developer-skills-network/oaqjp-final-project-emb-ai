7efca89f51eef9ebbae5bc777d96e5caf3022323
JackSavino <jack_savino@yahoo.com>
2023-10-03 17:51:30 -0400

    Adding updated file

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,23 +1,29 @@
-''' Module for Emotion Detection package
+''' Module for Emotion Detection package 
 '''
 import json
 import requests
 
 def emotion_detector(text_to_analyse):
-    ''' Function to run emotion detection of user text input
+    ''' Function to run emotion detection of user text input 
     '''
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/Nlp\
-    Service/EmotionPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['sadness']
-    dominant_emotion = max(list(zip(emotion.values(), emotion.keys())))[1]
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    emotion_predictor = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_predictor, key = emotion_predictor.get)
     return {
         'anger': anger_score,
         'disgust': disgust_score,
