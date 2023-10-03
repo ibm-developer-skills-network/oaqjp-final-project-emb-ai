ba64b0e5ca3257c4ef11dcc74c65103bfdc67d0b
JackSavino <jack_savino@yahoo.com>
2023-10-03 17:26:32 -0400

    Adding updated module from lab env

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,14 +1,29 @@
-''' Module for Emotion Detection package '''
+''' Module for Emotion Detection package
+'''
 
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
-    ''' Function to run emotion detection of user text input '''
-    
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp\
-    .v1/NlpService/EmotionPredict'
-    header = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en\
-    _stock'}
-    myobj = {'raw_document': {'text': text_to_analyse}}
+    ''' Function to run emotion detection of user text input
+    '''
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/Nlp\
+    Service/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(url, json = myobj, headers = header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
+        return {
+            'anger': emotion_predictions['anger'],
+            'disgust': emotion_predictions['disgust'],
+            'fear': emotion_predictions['fear'],
+            'joy': emotion_predictions['joy'],
+            'sadness': emotion_predictions['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        return None
