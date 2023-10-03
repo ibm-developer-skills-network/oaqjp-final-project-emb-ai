8709ff2532cd52d0c2915fc48feb49c0d2a2adde
JackSavino <jack_savino@yahoo.com>
2023-10-03 17:36:33 -0400

    Adding back the old file prior to changes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,29 +1,28 @@
 ''' Module for Emotion Detection package
 '''
-
 import json
 import requests
 
 def emotion_detector(text_to_analyse):
     ''' Function to run emotion detection of user text input
     '''
-
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/Nlp\
     Service/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
-    if response.status_code == 200:
-        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
-        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
-        return {
-            'anger': emotion_predictions['anger'],
-            'disgust': emotion_predictions['disgust'],
-            'fear': emotion_predictions['fear'],
-            'joy': emotion_predictions['joy'],
-            'sadness': emotion_predictions['sadness'],
-            'dominant_emotion': dominant_emotion
-        }
-    else:
-        return None
+    anger_score = formatted_response['emotionPredictions'][0]['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['sadness']
+    dominant_emotion = max(list(zip(emotion.values(), emotion.keys())))[1]
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
