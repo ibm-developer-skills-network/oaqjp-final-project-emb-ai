920b0d65ee4c626f413b1cbdcfa378bc55e36801
JackSavino <jack_savino@yahoo.com>
2023-10-04 11:55:41 -0400

    Adding updated files for final project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,24 +11,28 @@ def emotion_detector(text_to_analyse):
     myobj = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    emotion_predictor = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score
-    }
-    dominant_emotion = max(emotion_predictor, key = emotion_predictor.get)
-    return {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score,
-        'dominant_emotion': dominant_emotion
-    }
+
+    if response.status_code == 200:
+        emotion_predictor = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictor, key = emotion_predictor.get)
+        return {
+            'anger': emotion_predictor['anger'],
+            'disgust': emotion_predictor['disgust'],
+            'fear': emotion_predictor['fear'],
+            'joy': emotion_predictor['joy'],
+            'sadness': emotion_predictor['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    elif response.status_code == 400:
+        emotion_predictor = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictor, key = emotion_predictor.get)
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    else:
+        return None
--- server.py
+++ server.py
@@ -1,11 +1,10 @@
-''' Executing this function initiates the application of emotion
-    detection to be executed over the Flask channel and deployed on
-    localhost:5000.
+''' Executing this function initiates the application of emotion detection to be 
+    executed over the Flask channel and deployed on localhost:5000.
 '''
 # Import Flask, render_template, request from the flask framework package
 from flask import Flask, render_template, request
 
-# Import the sentiment_analyzer function from the package created
+# Import the emotion_detector function from the package created
 from EmotionDetection.emotion_detection import emotion_detector
 
 # Initiate the flask app
@@ -13,29 +12,35 @@ app = Flask('Emotion Detector')
 
 @app.route("/emotionDetector")
 def emot_detector():
-    ''' This code receives the text from the HTML interface and 
-        runs emotion detection over it using the emotion_detector()
-        function. The output returned shows the emotions, their 
-        associated scores, and the dominant emotion for the provided 
+    ''' This code receives the text from the HTML interface and runs emotion detection 
+        over it using the emotion_detector() function. The output returned shows the 
+        emotions, their associated scores, and the dominant emotion for the provided 
         text.
     '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Check if the input is blank or contains only whitespace
     if not text_to_analyze or text_to_analyze.strip() == "":
-        return 'Blank input! Please provide text to analyze.'
+        return None
     
     response = emotion_detector(text_to_analyze)
-    label = response['label']
-    score = response['score']
-    if label is None:
-        return 'Invalid input! Try again.'
-    return f"The given text has been identified as {label} with a score of {score}."
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    if dominant_emotion is None:
+        return 'Invalid text! Please try again!'
+    else:
+        return f"For the given statement, the system response is 'anger': {anger},\
+        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}.\
+        The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
+    ''' This function initiates the rendering of the main application page over the 
+        Flask channel
     '''
     return render_template('index.html')
 
